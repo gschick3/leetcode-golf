d9eb0ff50503a17c544d7970f547d5f3c81b2dbf
Galen Schick <gschick3@gmail.com>
2024-03-04 19:13:52 -0600

    Create solution.java

---
+++ two-sum/solution.java
@@ -0,0 +1,5 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        return IntStream.range(0, nums.length).mapToObj(num -> new int[] {num, IntStream.range(num+1, nums.length).filter(x -> nums[x] == (target - nums[num])).findFirst().orElse(-1)}).filter(x -> x[1] != -1).findFirst().orElse(new int[] {-1, -1});
+    }
+}
